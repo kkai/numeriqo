140b963d263dd2ca47647b445bbe1dbc2730b5d7
Kai Kunze <kaikunze@me.com>
2025-07-27 12:47:16 +0900

    Initial Commit

---
+++ gengen/gengenApp.swift
@@ -0,0 +1,17 @@
+//
+//  gengenApp.swift
+//  gengen
+//
+//  Created by kai on 27.07.25.
+//
+
+import SwiftUI
+
+@main
+struct gengenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
